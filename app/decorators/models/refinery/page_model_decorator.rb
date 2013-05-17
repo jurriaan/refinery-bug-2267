8dad167d7a94cd270756b36ebb7f28f2a4225fd3
Jurriaan Pruis <email@jurriaanpruis.nl>
2013-05-17 08:53:30 +0200

    comments/readme

--- app/decorators/models/refinery/page_model_decorator.rb
+++ app/decorators/models/refinery/page_model_decorator.rb
@@ -1,4 +1,6 @@
 puts "Decorating Refinery::Page"
+# Edit this file or routes.rb to 'undecorate' Refinery::Page
+# For example remove this comment.
 Refinery::Page.class_eval do
   def decorated?
     true
